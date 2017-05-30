65dda146fa8d22a400e86af6f70640d29777ae92
Jonas Schmitt <jonas.schmitt@fau.de>
2017-05-30 16:17:16 +0200

    Updated benchmark script

--- bench.sh
+++ bench.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
-cd impala
-make clean
+cd impala-build
 make
-./md 5e-5 1000 10000 > bench_impala.out
-cd ../cref
-make clean
+./md 5e-5 10000 100000 > bench_impala.out
+cd ../cref-build
 make
-./md 5e-5 1000 > 10000 > bench_cref.out
+./md 5e-5 10000 100000 > bench_cref.out
