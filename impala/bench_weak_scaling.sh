2df06c634cc7fd2a72253bebcf474192922e33e7
Jonas Schmitt <jonas.schmitt@fau.de>
2017-07-23 17:15:43 +0200

    Fixed impala weak scaling benchmark script

--- impala/bench_weak_scaling.sh
+++ impala/bench_weak_scaling.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sleep 10
-likwid-pin -q -c 0 ./md 5e-5 1000 500 1
-likwid-pin -q -c 0,2 ./md 5e-5 1000 1000 2
-likwid-pin -q -c 0,2,4 ./md 5e-5 1000 1500 3 
-likwid-pin -q -c 0,2,4,6 ./md 5e-5 1000 2000 4 
+likwid-pin -q -c 0 ./md 5e-5 1000 2500 1
+likwid-pin -q -c 0,2 ./md 5e-5 1000 5000 2
+likwid-pin -q -c 0,2,4 ./md 5e-5 1000 7500 3 
+likwid-pin -q -c 0,2,4,6 ./md 5e-5 1000 10000 4 
