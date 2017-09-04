e6e8bfcd21a4a26ea5bcdc11af3b231750663d34
Jonas Schmitt <jonas.schmitt@fau.de>
2017-09-04 15:49:53 +0200

    Removed obsolete scripts

--- scripts/lss-cluster.sh
+++ scripts/lss-cluster.sh
@@ -2,12 +2,13 @@
 #PBS -l nodes=4:ppn=32
 #PBS -l walltime=8:00:00
 #PBS -q normal 
-#PBS -M jonas.schmitt@fau.de -m abe
-#PBS -N md-4-nodes
+#PBS -M schmitt@localhost -m abe
+#PBS -N AnyDSL-MD
 
 . /etc/profile.d/modules.sh
 module load openmpi/1.10.4-gnu
 cd ~/Research/walberla_build/apps/anydsl-md/walberla
 make clean
 make
-mpirun -np 128 --bind-to socket ./md_simulation config_4_nodes.cfg 
+#mpirun -np 128 --bind-to socket ./md_simulation pe_cfg
+mpirun --map-by ppr:32:socket --bind-to socket ./md_simulation pe.cfg
