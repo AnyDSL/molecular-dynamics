cceec000f51a495814588e14e49ed8eb98ce06a1
Jonas Schmitt <jonas.schmitt@fau.de>
2017-09-08 11:20:17 +0200

    Refactoring

--- impala/configure.sh
+++ impala/configure.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cmake . -DAnyDSL-runtime_DIR=/software/anydsl/runtime -DLIKWID_DIR=/usr/local/likwid -DLIKWID_PERFMON=OFF -DCOUNT_COLLISIONS=OFF -DCHECK_INVARIANTS=OFF
+cmake . -DAnyDSL-runtime_DIR=/software/anydsl/runtime -DLIKWID_DIR=/usr/local/likwid -DLIKWID_PERFMON=OFF -DCOUNT_FORCE_EVALUATIONS=OFF -DCHECK_INVARIANTS=OFF
