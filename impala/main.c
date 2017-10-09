1d252cf4cf7f3c01227a2c5125d39a98e3d49abd
Jonas Schmitt <jonas.schmitt@fau.de>
2017-10-09 16:05:00 +0200

    Fixed walberla integration

--- impala/main.c
+++ impala/main.c
@@ -48,17 +48,17 @@ int main(int argc, char** argv) {
     //initialize_system(atol(argv[3]), l);
     real dt = atof(argv[1]);
     struct timeval t1, t2;
-    size_t const nsamples = 100;
-    //size_t const nsamples = 1;
+    //size_t const nsamples = 100;
+    size_t const nsamples = 1;
     double average = 0.0;
     double samples[nsamples];
     int nthreads = atoi(argv[4]);
     size_t np = atol(argv[3]);
-    for(size_t i = 0; i < 50; ++i) { 
+    /*for(size_t i = 0; i < 50; ++i) { 
         initialize_system(np, l);
         time_integration(0.0, atol(argv[2])*dt, dt, nthreads, vtk);
         deallocate_system();
-    }
+    }*/
 
 
     for(size_t i = 0; i < nsamples; ++i) { 
