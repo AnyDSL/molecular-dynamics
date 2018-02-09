348fd2f2c1b2823fbe077f49ed51d268b689a6e5
Jonas Schmitt <jonas.schmitt@fau.de>
2018-02-09 18:16:09 +0100

    Now invalid interactions are masked out. The GPU version is still incorrect

--- neighborlists/main.cpp
+++ neighborlists/main.cpp
@@ -71,7 +71,7 @@ int main(int argc, char **argv) {
     }
     
     // Body Collision Test
-    /*double potential_minimum = std::pow(2.0, 1.0/6.0) * sigma;
+    double potential_minimum = std::pow(2.0, 1.0/6.0) * sigma;
     std::cout << "Potential minimum at: " << potential_minimum << std::endl;
 
     AABB aabb1;
@@ -90,7 +90,7 @@ int main(int argc, char **argv) {
     }
     double shift = potential_minimum + (aabb2.max[1] - aabb2.min[1]);
     aabb2.min[1] -= shift;
-    aabb2.max[1] -= shift;*/
+    aabb2.max[1] -= shift;
     
     std::vector<double> grid_initialization_time(runs, 0);
     std::vector<double> copy_data_to_accelerator_time(runs, 0);
@@ -111,8 +111,8 @@ int main(int argc, char **argv) {
 
     for(int i = 0; i < runs; ++i) {
         auto begin = measure_time();
-        int size = init_rectangular_grid(static_cast<unsigned>(i), aabb, spacing, maximum_velocity, cutoff_radius+verlet_buffer, 2048);
-        //int size = init_body_collision(0, aabb1, aabb2, spacing1, spacing2, 1, 1, 100, 5, 2048);
+        //int size = init_rectangular_grid(static_cast<unsigned>(i), aabb, spacing, maximum_velocity, cutoff_radius+verlet_buffer, 2048);
+        int size = init_body_collision(0, aabb1, aabb2, spacing1, spacing2, 1, 1, 100, 5, 2048);
         auto end = measure_time();
         grid_initialization_time[i] = static_cast<double>(calculate_time_difference<std::chrono::nanoseconds>(begin, end))*factor;
 
@@ -156,7 +156,7 @@ int main(int argc, char **argv) {
             end = measure_time();
             position_integration_time[i] += static_cast<double>(calculate_time_difference<std::chrono::nanoseconds>(begin, end))*factor;
             
-            if(j > 0 && j % 20 == 0) {
+            //if(j > 0 && j % 20 == 0) {
 
                 begin = measure_time();
                 md_copy_data_from_accelerator();
@@ -182,7 +182,7 @@ int main(int argc, char **argv) {
                 md_copy_data_to_accelerator();
                 end = measure_time();
                 copy_data_to_accelerator_time[i] = static_cast<double>(calculate_time_difference<std::chrono::nanoseconds>(begin, end))*factor;
-            }
+            //}
 
 
 	    LIKWID_MARKER_START("Force");
