2450977b1a75e3ce5a404db8743b3f5cc39edba4
Rafael Ravedutti <rafael@laura-br.com>
2018-10-31 00:24:05 +0100

    Add base files for MPI
    Signed-off-by: Rafael Ravedutti <rafael@laura-br.com>

---
+++ neighborlists/mpi.cpp
@@ -0,0 +1,17 @@
+#include <mpi.h>
+
+extern "C" {
+
+int MPI_init() { return MPI_Init(0, 0); }
+
+MPI_Op get_mpi_max() { return MPI_MAX; }
+MPI_Op get_mpi_sum() { return MPI_SUM; }
+
+MPI_Datatype get_mpi_int() { return MPI_INT; }
+MPI_Datatype get_mpi_double() { return MPI_DOUBLE; }
+
+MPI_Comm get_mpi_comm_world() { return MPI_COMM_WORLD; }
+
+MPI_Status* get_mpi_status_ignore() { return MPI_STATUS_IGNORE; }
+
+}
