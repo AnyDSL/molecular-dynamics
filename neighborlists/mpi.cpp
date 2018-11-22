a85951e96158d3912b8a4225f1f7bacffd7e5ae8
Rafael Ravedutti <rafaelravedutti@gmail.com>
2018-11-22 16:43:58 +0100

    Add MPI float data type
    Signed-off-by: Rafael Ravedutti <rafaelravedutti@gmail.com>

--- neighborlists/mpi.cpp
+++ neighborlists/mpi.cpp
@@ -9,6 +9,7 @@ MPI_Op get_mpi_sum() { return MPI_SUM; }
 
 MPI_Datatype get_mpi_int() { return MPI_INT; }
 MPI_Datatype get_mpi_double() { return MPI_DOUBLE; }
+MPI_Datatype get_mpi_float() { return MPI_FLOAT; }
 
 MPI_Comm get_mpi_comm_world() { return MPI_COMM_WORLD; }
 
